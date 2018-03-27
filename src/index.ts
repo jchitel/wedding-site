fd9786b25ee6747c4d5c4ace278f5a8b27e03f92
Jake Chitel <jchitel@gmail.com>
2018-03-27 00:01:05 -0500

    bumping up lambda timeout, turning off ignoring for ts-node

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export const handler = (event: any, context: any, callback: any) => {
     Error.stackTraceLimit = Infinity;
     const tsNode = require('ts-node');
     // register ts-node
-    tsNode.register({ project: `${__dirname}/tsconfig.json`, ignore: /\/node_modules\/(?!koa-mount)/ });
+    tsNode.register({ project: `${__dirname}/tsconfig.json`, skipIgnore: true });
 
     const serverless = require('serverless-http').default;
     const app = require('./server').default;
