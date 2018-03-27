8de381cb83e07addc9e2a72a3064a13815bcc8f4
Jake Chitel <jchitel@gmail.com>
2018-03-27 00:08:42 -0500

    bumping up lambda memory, moving logic back to outside the handler

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,13 @@
-/*Error.stackTraceLimit = Infinity;
+Error.stackTraceLimit = Infinity;
 import * as tsNode from 'ts-node';
 // register ts-node
-tsNode.register({ project: `${__dirname}/tsconfig.json` });
+tsNode.register({ project: `${__dirname}/tsconfig.json`, skipIgnore: true } as any);
 
 import serverless from 'serverless-http';
 import app from './server';
 
 
-/** export lambda hander wrapped around koa app * /
+/** export lambda hander wrapped around koa app */
 export const handler = serverless(app, {
     callbackWaitsForEmptyEventLoop: true
-});*/
-
-export const handler = (event: any, context: any, callback: any) => {
-    Error.stackTraceLimit = Infinity;
-    const tsNode = require('ts-node');
-    // register ts-node
-    tsNode.register({ project: `${__dirname}/tsconfig.json`, skipIgnore: true });
-
-    const serverless = require('serverless-http').default;
-    const app = require('./server').default;
-
-
-    /** export lambda hander wrapped around koa app */
-    const _handler = serverless(app, {
-        callbackWaitsForEmptyEventLoop: true
-    });
-    _handler(event, context, callback);
-}
+});
