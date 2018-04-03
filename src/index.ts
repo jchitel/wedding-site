01da437312d0c12e5025adf6c1e138ea9939ce83
Jake Chitel <jchitel@gmail.com>
2018-04-03 00:00:29 -0500

    explicitly setting binary content types

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import app from './server';
 
 /** export lambda hander wrapped around koa app */
 export const handler = serverless(app, {
-    callbackWaitsForEmptyEventLoop: true
+    callbackWaitsForEmptyEventLoop: true,
+    binary: ['image/png', 'image/jpeg', 'application/x-font-ttf']
 });
