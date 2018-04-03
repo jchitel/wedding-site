60e9512ac54fbc48177d79f87a994788e8794d93
Jake Chitel <jchitel@gmail.com>
2018-04-03 00:50:01 -0500

    switched from serverless-http to aws-serverless-express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 Error.stackTraceLimit = Infinity;
-import serverless from 'serverless-http';
+import serverless from 'aws-serverless-express';
+import * as lambda from 'aws-lambda';
 import app from './server';
 
 
-/** export lambda hander wrapped around koa app */
-export const handler = serverless(app, {
-    callbackWaitsForEmptyEventLoop: true,
-    binary: ['image/png', 'image/jpeg', 'application/x-font-ttf']
-});
+/** export lambda hander wrapped around express app */
+const server = serverless.createServer(app, undefined, ['image/png', 'image/jpeg', 'application/x-font-ttf']);
+export const handler: lambda.APIGatewayProxyHandler = (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = true;
+    console.log('Handling event:', event);
+    serverless.proxy(server, event, {
+        ...context,
+        succeed: (value: any) => {
+            console.log('Response:', value);
+            context.succeed(value);
+        }
+    });
+}
