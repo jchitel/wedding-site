3f15dc9a4d4d5d188d54c02ac03ee32b0f95b843
Jake Chitel <jchitel@gmail.com>
2018-04-02 23:33:56 -0500

    added switch for choosing production vs build bundle path

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,20 @@ import path from 'path';
 
 const app = express();
 
+// request logging
+app.use((req, _res, next) => {
+    console.log(
+`${req.method.toUpperCase()} ${req.path} HTTP/1.1
+${req.rawHeaders.reduce((str, _, i) => i % 2 === 0 ? `${str}${_}` : `${str}: ${_}\n`, '')}
+${req.body}`
+    );
+    next();
+});
+
+// in lambda, the __dirname will be /var/task.
+const isLambda = __dirname === '/var/task/';
 // static files
-app.use(express.static(path.resolve(__dirname, '..', 'build', 'bundle')));
+const parts = isLambda ? [__dirname, 'bundle'] : [__dirname, '..', 'build', 'bundle'];
+app.use(express.static(path.resolve(...parts)));
 
 export default app;
