eb266432dd6386042fe2a41f0a184ff75e44685d
Jake Chitel <jchitel@gmail.com>
2018-04-02 23:39:00 -0500

    small fixes

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ const app = express();
 
 // request logging
 app.use((req, _res, next) => {
-    console.log(
-`${req.method.toUpperCase()} ${req.path} HTTP/1.1
+    console.log(`
+${req.method.toUpperCase()} ${req.path} HTTP/1.1
 ${req.rawHeaders.reduce((str, _, i) => i % 2 === 0 ? `${str}${_}` : `${str}: ${_}\n`, '')}
 ${req.body}`
     );
@@ -15,7 +15,7 @@ ${req.body}`
 });
 
 // in lambda, the __dirname will be /var/task.
-const isLambda = __dirname === '/var/task/';
+const isLambda = __dirname === '/var/task';
 // static files
 const parts = isLambda ? [__dirname, 'bundle'] : [__dirname, '..', 'build', 'bundle'];
 app.use(express.static(path.resolve(...parts)));
