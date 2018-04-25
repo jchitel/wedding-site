8a13a2100c795784c4c96187cb9de0ffa7626d48
Jake Chitel <jchitel@gmail.com>
2018-04-25 15:20:34 -0500

    removed map from table of contents

--- packages/wedding-site-client/src/components/toc.tsx
+++ packages/wedding-site-client/src/components/toc.tsx
@@ -8,12 +8,10 @@ export default function Toc() {
             <span className="toc" style={{ ...centerText, fontSize: '2em', width: '60%', margin: 'auto', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
                 <a className="toc-link" href="#venue">Venue</a>
                 <a className="toc-link" href="#hotel">Hotel</a>
-                <a className="toc-link" href="#map">Map</a>
                 <a className="toc-link" href="#registry">Registries</a>
                 <a className="toc-link" href="#rsvp">RSVP</a>
                 <a className="toc-link" href="#couple">Bride &amp; Groom</a>
                 <a className="toc-link" href="#party">Wedding Party</a>
-                {/*<a className="toc-link" href="#pics">Pictures</a>*/}
             </span>
         </>
     );
