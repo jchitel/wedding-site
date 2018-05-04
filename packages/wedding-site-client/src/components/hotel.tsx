143d60d5ff155cdda8775f6b77932b1d84b77ea5
Jake Chitel <jchitel@gmail.com>
2018-05-04 12:19:01 -0500

    Changing hotel link

--- packages/wedding-site-client/src/components/hotel.tsx
+++ packages/wedding-site-client/src/components/hotel.tsx
@@ -9,7 +9,7 @@ export default function Hotel() {
             <span style={{ ...centerText, fontSize: '2.5em' }}>Fairfield Inn &amp; Suites Milwaukee Downtown</span>
             <span style={{ ...centerText, fontSize: '1.5em' }}>710 N Old World Third St, Milwaukee, Wisconsin 53203</span>
             <span style={{ ...centerText, fontSize: '2em' }}>
-                <a href="http://cwp.marriott.com/mkefd/heimchitelwedding/" target="_blank">Reserve your room here!</a>
+                <a href="http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Heim%20%26%20Chitel%20Wedding%5Emkefd%60HEIHEIK%7CHEIHEIQ%60169.00%60USD%60false%604%606/22/18%606/24/18%606/1/18&app=resvlink&stop_mobi=yes" target="_blank">Reserve your room here!</a>
             </span>
         </>
     );
