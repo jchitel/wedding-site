737ce4815b185864ba7477dd75928b8cb2bb6e3f
Jake Chitel <jchitel@gmail.com>
2022-05-02 15:51:30 -0500

    tring to figure out what filter to put on the pic

--- src/pages/newindex.tsx
+++ src/pages/newindex.tsx
@@ -26,8 +26,10 @@ function HeroCard() {
                             className="h-full w-full object-cover"
                             src={coupleImage}
                             alt="The adorable wedding couple"
+                            layout="fill"
                         />
-                        <div className="absolute inset-0 bg-yellow-700 mix-blend-soft-light" />
+                        <div className="absolute inset-0 bg-blue-900 mix-blend-hue" />
+                        {/* <div className="absolute inset-0 bg-yellow-700 mix-blend-color" /> */}
                     </div>
                     <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                         <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
